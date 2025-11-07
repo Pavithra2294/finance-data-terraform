977c518b9276c18ce2ff5869cbd46c599d1326cb
Pavithra2294 <pav.ganesan@gmail.com>
2025-11-07 20:19:55 +0000

    Update buckets.tf

--- buckets.tf
+++ buckets.tf
@@ -4,13 +4,6 @@ resource "aws_s3_bucket" "source-data-bucket-pre" {
   force_destroy = true
 }
 
-
-resource "aws_s3_object" "source-data-bucket-pre" {
-  bucket = aws_s3_bucket.source-data-bucket-pre.id
-  key    = "sample_financial_data_100_extended.csv"           
-  source = "C:/Users/ashok/OneDrive/Desktop/finance-data-terraform/sample_financial_data_100_extended.csv"
-}
-
 # S3 Bucket for Traget 
 resource "aws_s3_bucket" "finance-data-target-pre" {
   bucket        = "finance-data-target-pre"
@@ -28,6 +21,7 @@ resource "aws_s3_object" "glue-code-bucket-pre" {
   key    = "finance_data.py"           
   source = "C:/Users/ashok/OneDrive/Desktop/finance-data-terraform/finance_data.py" 
 }
+
 
 
 
